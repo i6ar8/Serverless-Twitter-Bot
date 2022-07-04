2ad5fb9b31ab0932627933fdbb7fec2a68283fd1
Tariq <34150198+tariq-alharbi@users.noreply.github.com>
2022-07-04 16:05:57 +0300

    add comment.

--- lambda_function.py
+++ lambda_function.py
@@ -2,9 +2,9 @@ import json
 from tweetNflAr import tweetNflAr
 import os
 def lambda_handler(event, context):
-    
+    #getting the consumer key, consumer_secret,access_token, and access_token_secret from the environment variables and sending it to the tweetNflAr constructor
     tweetNflAr(os.environ.get("consumer_key"),os.environ.get("consumer_secret"),os.environ.get("access_token"),os.environ.get("access_token_secret"))
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        
     }
